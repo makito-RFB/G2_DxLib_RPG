0c8ba85ff06d20ee3b26c32a61a7e5568beb56bc
makito-RFB <hj-g19016@sist.ac.jp>
2020-09-23 09:59:52 +0900

    mapを変更

---
+++ IMAGE/MAP/map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="map1" tilewidth="32" tileheight="32" tilecount="2624" columns="64">
+ <image source="mapchip1.png" width="2048" height="1312"/>
+</tileset>
